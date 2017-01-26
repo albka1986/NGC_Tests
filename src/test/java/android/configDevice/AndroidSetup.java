773bf21723b1c98ee140b1c816e0304568dcbe39
Ponomarenko Oleh <albka1986@gmail.com>
2017-01-26 17:58:04 +0200

    updated for sprint 7

--- src/test/java/android/configDevice/AndroidSetup.java
+++ src/test/java/android/configDevice/AndroidSetup.java
@@ -11,9 +11,9 @@ import java.net.URL;
 public class AndroidSetup {
 
     public static final String APP_PACKAGE_NAME = "com.erminesoft.ngc:id/";
-    public final static String sprint = "6";
-    public final static File BUILD_PATH = new File("/Users/oleh/Documents/App/NGC/Android/build/sprint_" + sprint);
-    public final static String BUILD_NAME = "Campfiire-0.6.4-qaDebug.apk";
+    public final static String sprint = "7";
+    public final static File BUILD_PATH = new File("//Users/oleg/Google Drive/QA DRIVE/Applications/NGC/sprint " + sprint);
+    public final static String BUILD_NAME = "Campfiire-0.7.5-prodDebug.apk";
     public final static File app = new File(BUILD_PATH, BUILD_NAME);
     public static final String DEVICE_MOTOROLA = "ZX1B24GVX2";
     public static AndroidDriver driver;
